c6675507c8ba52430d07c6f411440dd3cfbaac73
huxiaoman7 <charlotte77_hu@sina.com>
2017-11-01 10:50:15 +0800

    add zero padding func

--- 1.mnist/conv.py
+++ 1.mnist/conv.py
@@ -29,4 +29,16 @@ class Conv:
 	# 判断水平方向上的卷积层输出的神经元个数为整数
 	assert ((w - k_x + 2 * p_x) % s_x ==0)
 	# 判断垂直方向上的卷积层输出的神经元个数为整数
-	assert ((w - k_y + 2 * p_y) % s_y ==0)
+	assert ((h - k_y + 2 * p_y) % s_y ==0)
+	self.w_num = (w - k_x + 2 * p_x) % s_x) + 1
+	self.h_num = (h - k_y + 2 * p_y) % s_y) + 1
+	self.weights = np.random.randn(f, c * k_x * k_y) / nq.sqrt(c * k_x * k_y)
+	self.bias = np.random.randn(f)
+	self.lr,self.lamb = 0.0, 0.0
+
+    # zero padding
+    def padding(self, x):
+	zero_num = x.shape
+	zero = np.zeros((zero_num[0], zero_num[1], zero_num[2] + 2* self.p_x, zero_num[3] + 2* self.p_y))
+	x = zero[:, :, self.p_x:self.p_x + self.w, self.p_y:self.p_y + self.h]
+	return zero
